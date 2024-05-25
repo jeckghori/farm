955efb92849614c8691490e759fa9bda5c5244d7
jeckghori <158252729+jeckghori@users.noreply.github.com>
2024-05-25 18:39:52 +0530

    Add files via upload

---
+++ farm.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import pickle
+
+st.title("crop price ")
+
+df = pickle.load(open('df-3.pkl','rb'))
+pipe = pickle.load(open('pipe-3.pkl','rb'))
+
+X = df.drop('Price_per_KG_INR',axis=1)
+y = df.iloc[:,-1]
+
+state = st.selectbox('Select State:',df['State'].unique())
+
+dist = st.selectbox('Select District:',df['District'].unique())
+
+year = st.selectbox('Select Year:',df['Crop_Year'].unique())
+
+season = st.selectbox('Season:',df['Season'].unique())
+
+crop = st.selectbox('Select Crop:',df['Crop'].unique())
+
+Area = st.number_input(label='Total Area:',step=1,format='%.2d',min_value=0,max_value=1250)
+
+
+Production = st.number_input(label='Total Production:',step=1,format='%.2d',min_value=0,max_value=1250)
+
+btn = st.button('Predict Price')
+
+if btn:
+    query = pd.DataFrame(data=[[state,dist,year,season,crop,Area,Production]],columns=X.columns)
+    st.title('Predicted Price is:' +str(round(pipe.predict(query)[0],2)))
